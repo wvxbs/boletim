3a95bb5b3253564bfd7c030dd61f4d15a5d4e7b0
wvxbs <gabriel.ferreira7854@gmail.com>
2022-08-19 17:14:20 -0300

    started working on the home screen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -68,20 +68,7 @@ function BottomTabNavigator() {
         options={({ navigation }: RootTabScreenProps<'Home'>) => ({
           title: 'Home',
           tabBarIcon: ({ color }) => <TabBarIcon name="graduation-cap" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
+          headerShown: false
         })}
       />
       <BottomTab.Screen
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -7,26 +7,43 @@ import { RootTabScreenProps } from '../types';
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/Home.tsx" />
+      <Text style={styles.title}>
+        Boa Tarde!
+      </Text>
+      <Card />
     </View>
   );
 }
 
+const Card = () => {
+  return(
+    <View style={styles.card}>  
+      <Text style={styles.body}>
+        Exemplo
+      </Text>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    padding: 15,
+    height: '100%'
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 22
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  card: {
+    height: 300
+  }
 });
